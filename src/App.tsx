e7f8509896d48b5e5fed5f91f365b278eb614792
Evgeniy <y.danchanka@tapston.com>
2023-06-04 23:43:57 +0300

    add lazy component

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {Suspense} from 'react'
+import {Route, Routes, Link} from "react-router-dom";
+import './styles/index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;

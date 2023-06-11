7bc9756260c7967f16c0310af523d669f4048509
Evgeniy <y.danchanka@tapston.com>
2023-06-11 16:07:56 +0300

    add new loaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,34 +1,49 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import type { RuleSetRule } from 'webpack';
-import { BuildOptions } from './types/config';
+import type {RuleSetRule} from 'webpack';
+import {BuildOptions} from './types/config';
 
-export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
+export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Creates `style` nodes from JS strings
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            auto: (resPath: string) =>
-              Boolean(resPath.includes('.module.')),
-            localIdentName: isDev
-              ? '[path][name]__[local]--[hash:base64:5]'
-              : '[hash:base64:5]',
-          },
-        },
-      },
-      'sass-loader',
-    ],
-  };
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
 
-  return [typescriptLoader, cssLoader];
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) =>
+                            Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:5]',
+                    },
+                },
+            },
+            'sass-loader',
+        ],
+    };
+
+    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
 }

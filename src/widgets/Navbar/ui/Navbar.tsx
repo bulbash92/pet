81c37c4f47f81ff3a3aeb5340a1ec8c08a59c2d1
Evgeniy <y.danchanka@tapston.com>
2023-06-12 21:24:07 +0300

    add navbar, add routerConfig

---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,19 @@
+import {classNames} from 'helpers/classNames';
+import classes from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(classes.Navbar, {}, [className])}>
+            <div className={classes.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={classes.mainLink}>Главная</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О сайте</AppLink>
+            </div>
+        </div>
+    );
+};

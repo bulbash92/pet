6dd3cc93ad01b2580f24b0bdb6a6c554206c6132
Evgeniy <evgeniydanchenko92@gmail.com>
2023-07-17 15:31:31 +0300

    fix theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React, { ReactNode, useCallback, useEffect } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
 import classes from './Modal.module.scss';
-import { useTheme } from 'app/providers/ThemeProvider';
 
 interface ModalProps {
     className?: string;
@@ -20,8 +19,6 @@ export const Modal = ({
     const mods: Record<string, boolean> = {
         [classes.opened]: isOpen,
     };
-
-    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -51,7 +48,7 @@ export const Modal = ({
 
     return (
         <Portal>
-            <div className={classNames(classes.Modal, mods, [className, theme])}>
+            <div className={classNames(classes.Modal, mods, [className])}>
                 <div className={classes.overlay} onClick={closeHandler}>
                     <div className={classes.content} onClick={onContentClick}>
                         {children}
